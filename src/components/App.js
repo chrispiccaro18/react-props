a47d0d99515fd7888d35b7dee7d3762351cb9c1c
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-13 16:44:59 -0700

    header test passing

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './Header';
 import Dog from './Dog';
 
 export default function App() {
   return (
     <>
+    <Header />
     <Dog name="Joe" age={10} weight="20 lbs" />
     </>
   );
