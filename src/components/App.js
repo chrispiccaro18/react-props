3cd736094f26df777b78763157c75a739aac450e
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-13 17:54:09 -0700

    liking the color componenet

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Color from './Color';
+
+export default function App() {
+  return (
+    <>
+    <Color name='sky blue' rgb={{ red: 135, green: 206, blue: 235 }}/>
+    </>
+  );
+}
---
+++ src/components/Color.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Color({ name, rgb }) {
+  const hex = '#' + rgb.red.toString(16).toUpperCase() + rgb.green.toString(16).toUpperCase() + rgb.blue.toString(16).toUpperCase();
+  
+  const colorPreview = {
+    backgroundColor: hex,
+    display: 'inline-Block',
+    width: '0.8em',
+    height: '0.8em'
+  };
+
+  return (<dl>
+    <dt>Name</dt>
+    <dd>
+      {name ? name : hex}
+      <div style={colorPreview}></div>
+    </dd>
+
+    <dt>Hex</dt>
+    <dd>{hex}</dd>
+
+    <dt>RGB</dt>
+    <dd>Red - {rgb.red}</dd>
+    <dd>Green - {rgb.green}</dd>
+    <dd>Blue - {rgb.blue}</dd>
+  </dl>
+  );
+}
+
+Color.propTypes = {
+  name: PropTypes.string,
+  rgb: PropTypes.object.isRequired
+};
+
+export default Color;
